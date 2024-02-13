52c0703d2e0395297084cb68840d7d31e9fda626
Cesar Sanchez <mekoxplay@gmail.com>
2024-02-13 09:33:03 -0500

    Co-authored-by: Zkumbrig <Zkumbrig@users.noreply.github.com> Co-authored-by: EstrellitaC <EstrellitaC@users.noreply.github.com>

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-const slides = document.query
+const slides = document.querySelectorAll('.container-slider');
+const indicators = document.querySelectorAll( '.slider-indicators li' );
+let currentSlide = 0;
+
+function showSlide(index){
+    slides.forEach(slide => {
+        slide.style.opacity = '0';
+    });
+    slides[index].style.opacity = '1'
+}
