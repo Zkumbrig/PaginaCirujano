39c64df66019f679d99e7a4c2a2437f44490b428
Cesar Sanchez <mekoxplay@gmail.com>
2024-02-13 10:04:04 -0500

    Co-authored-by: Zkumbrig <Zkumbrig@users.noreply.github.com> Co-authored-by: EstrellitaC <EstrellitaC@users.noreply.github.com>

--- js/app.js
+++ js/app.js
@@ -1,8 +1,30 @@
-const buttons = [
+let sliderButtons = [
     document.getElementById('btnSlider1'),
     document.getElementById('btnSlider2'),
     document.getElementById('btnSlider3')
-]
-
-let totalSlides = document.querySelectorAll('.container-slide' > li).length;
+];
+let sliderContainer = document.querySelector('.container-slider');
+let slides = document.querySelectorAll('.container-slider > li');
+let totalSlides = slides.length;
 let currentIndex = 0;
+
+function updatePosition() {
+    slides.forEach((slide, index) => {
+        slide.style.opaci
+    })
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    updatePosition();
+}
+
+sliderButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        currentIndex = index;
+        updatePosition();
+    });
+});
+setInterval(nextSlide, 5000);
+
+updatePosition();
