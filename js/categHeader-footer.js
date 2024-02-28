5a080e953495a657b36e9dbed1720a1d2038b97d
Cesar Sanchez <mekoxplay@gmail.com>
2024-02-28 11:42:38 -0500

    ahora si el ultimo commit de links

--- js/categHeader-footer.js
+++ js/categHeader-footer.js
@@ -30,7 +30,7 @@ const newHtml = `
 <section class="navbar" id="navBar">
     <div class="container navbar__content">
         <div class="logoDr">
-            <a href="/index.html"><img src="/img/logo.webp" alt=""></a>
+            <a href="../index.html"><img src="../img/logo.webp" alt=""></a>
         </div>
         <div class="menu-icon">
             <p>Menu <i class="fa-solid fa-bars"></i></p>
@@ -38,15 +38,15 @@ const newHtml = `
         <div class="nav">
             <ul>
                 <div class="logoDr">
-                    <a href="index.html"><img src="/img/logo.webp" alt=""></a>
+                    <a href="../index.html"><img src="../img/logo.webp" alt=""></a>
                 </div>
-                <li><a href="/index.html">Inicio</a></li>
-                <li><a href="/about.html">Su Cirujano</a></li>
-                <li><a href="/categoriaquirurgicos.html">Procedimientos Quirúrjicos</a></li>
-                <li><a href="/categorianoquirurgicos.html">No Quirúrjicos</a></li>
-                <li><a href="/galeria.html">Galería</a></li>
-                <li><a href="/blog.html">Blog</a></li>
-                <a href="/contact.html" class="btn-nav" ><i class="fa-regular fa-calendar"></i><p>Contactanos</p></a>
+                <li><a href="../index.html">Inicio</a></li>
+                <li><a href="../about.html">Su Cirujano</a></li>
+                <li><a href="../categoriaquirurgicos.html">Procedimientos Quirúrjicos</a></li>
+                <li><a href="../categorianoquirurgicos.html">No Quirúrjicos</a></li>
+                <li><a href="../galeria.html">Galería</a></li>
+                <li><a href="../blog.html">Blog</a></li>
+                <a href="../contact.html" class="btn-nav" ><i class="fa-regular fa-calendar"></i><p>Contactanos</p></a>
             </ul>
         </div>
     </div>
@@ -55,7 +55,7 @@ const newHtml = `
 const newFooter = `<div class="container footer-content">
 <div class="row-footer">
     <section class="dr-footer-section">
-        <img src="/img/logo-blanco-footer.webp" alt="">
+        <img src="../img/logo-blanco-footer.webp" alt="">
         <p>Cirujano Plástico Certificado especializado en BRASIL y EEUU</p>
     </section>
     <section class="ubicanos-footer">
